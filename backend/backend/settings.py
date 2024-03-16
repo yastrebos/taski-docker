08f3b0dffb758b5fe5e87e050c3fe1050930b338
Сергей Ястребов <yastrebos@vk.com>
2024-03-16 12:06:15 +0300

    Add workflow with PEP8 linting2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['tasks.servequake.com','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['tasks.servequake.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
